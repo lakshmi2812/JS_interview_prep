f616ccd06ea37475c6528b8400e4494833fb0e72
lakshmi2018 <lakshmi.nishthala@gmail.com>
2018-07-28 16:35:05 -0700

    Understanding self in JS

--- VanillaJS/Mock_interview1/q8.js
+++ VanillaJS/Mock_interview1/q8.js
@@ -12,17 +12,33 @@
 //Q. Fix obj to return 1 as output for line 11
 
 //Solution 1: Make inner an arrow function
+// const obj = {
+//   x:1,
+//   getX:function(){
+//     const inner = ()=>{
+//       console.log(this.x);
+//     }
+//     inner();
+//   }
+// };
+// obj.getX();
+// //Solution 2: use call, apply or bind
+// obj.getX.call(obj)
+// obj.getX.apply(obj)
+// obj.getX.bind(obj)
+
+
+//self=this Solution
+
 const obj = {
   x:1,
   getX:function(){
-    const inner = ()=>{
-      console.log(this.x);
+    var self = this;
+    const inner = function(){
+      console.log(self.x)
     }
-    inner();
+    inner()
   }
-};
+}
+
 obj.getX();
-//Solution 2: use call, apply or bind
-obj.getX.call(obj)
-obj.getX.apply(obj)
-obj.getX.bind(obj)
